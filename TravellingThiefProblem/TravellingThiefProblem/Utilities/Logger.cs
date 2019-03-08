5ae81f503256640728204e6ce1a2e4f42b1c975d
maciej <cwiakowski.maciej@gmail.com>
2019-03-08 20:20:30 +0100

    Implemented Logger

--- TravellingThiefProblem/TravellingThiefProblem/Utilities/Logger.cs
+++ TravellingThiefProblem/TravellingThiefProblem/Utilities/Logger.cs
@@ -1,4 +1,7 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
 
 namespace TravellingThiefProblem.Utilities
 {
@@ -9,5 +12,43 @@ namespace TravellingThiefProblem.Utilities
 
         // px od 0.5 do 1/       0.3, 1
         // pm 0.01 do 0.1       0,3, 0,7
+
+        public List<string> stats { get; set; }
+
+        public Logger()
+        {
+            stats = new List<string>();
+        }
+
+        public string GenerateFileName(string source)
+        {
+            var name = $"{DateTime.Now.ToString("yyyyMMdd_HHmmss")}_{source.Substring(5, source.Length-9)}";
+
+            return name;
+        }
+
+        public void AddToLogger(int generationNumber, double bestFitness, double averageFitness, double worstFitness)
+        {
+            stats.Add($"{generationNumber}, {bestFitness.ToString(CultureInfo.InvariantCulture)}, {averageFitness.ToString(CultureInfo.InvariantCulture)}, {worstFitness.ToString(CultureInfo.InvariantCulture)}");
+        }
+
+        public void SaveToFile(string source)
+        {
+            string path = $@"D:\Projekty\SIiIW\TravellingThiefProblem\TravellingThiefProblem\Data\Output\{GenerateFileName(source)}.txt";
+
+            if (!File.Exists(path))
+            {
+                File.Create(path).Dispose();
+
+                using (TextWriter tw = new StreamWriter(path))
+                {
+                    foreach (var stat in stats)
+                    {
+                        tw.WriteLine(stat);
+                    }
+                }
+
+            }
+        }
     }
 }
